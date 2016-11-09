12fae422429c7235f32db769995ed975a714b97c
pdk7 <pdk7@pitt.edu>
2016-11-08 23:28:21 -0500

    Make final changes

--- js/quiz2.js
+++ js/quiz2.js
@@ -6,17 +6,18 @@
 //  - finally, write a function that makes you happy :D (as in, have some fun a write something cool, it's up to you!)
 
 (function($){
-	$('.links li a').on('click', function(e) {
-		e.preventDefault()
-		alert('you clicked a link, good for you');
-	});
+  $('.links li a').on('click', function(e) {
+    e.preventDefault()
+      alert('you clicked a link, good for you');
+  });
 
-	console.log("this is a message for you!!!")
+  console.log("this is a message for you!!!")
 })(jQuery);
 
 function alter_li() {
   var lis = $('ol > li');
-  lis.attr('color', 'red');
+  lis.css("color", "red");
+
   for (var i = 0; i < lis.length; i++) {
     var li = lis.get(i);
     li.innerHTML = "Hey there! We are list item #" + (i + 1);    
@@ -27,12 +28,38 @@ function change_body() {
   $('body').get(0).innerHTML = "This is the end of the world ...";
 }
 
+//Click an element in the list to switch it to the other list!
+function list_switcharoo() {
+  if ($(this).parent().is('ul')) {
+    $(this).remove();
+    $(this).appendTo($('ol'));
+  }
+  else {
+    $(this).remove();
+    $(this).appendTo($('ul'));
+  }
+  $(this).click(list_switcharoo);
+  $(this).get(0).innerHTML += " Stahp!";
+
+}
+
 $(document).ready(function(e) {
   alter_li();
+
   $('#danger').on('click', function(e) {
     $('#fadeAway').fadeOut(2000, function(e) {$('#fadeIn').fadeIn(1000)});
   });
-  $('ul > li').on('click', function(e) {
-    $(this).get(0).innerHTML += " Stahp!";
+
+  $('li').on('click', list_switcharoo);
+
+  $('.accordion-label').click(function(e) {
+    if($('.accordion-panel > p').css('opacity') === '0') {
+      $(this).css('opacity', '.8');
+      $('.accordion-panel > p').fadeTo(500, 1);
+      $('.accordion-panel').show(500);
+    } else {
+      $(this).css('opacity', '1'); 
+      $('.accordion-panel > p').fadeTo(50, 0, function(e) {$('.accordion-panel').hide(500);});
+    }
   });
 });
